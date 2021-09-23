204e4654e87bb0c01b7749d7d13968bea4fcc7d3
Edwin Game Dev <edwinjonesholanda@gmail.com>
2021-09-23 18:45:44 -0300

    Created bubble + added background sprites

---
+++ Assets/_Main/Bubble/Bubble.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bubble : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
