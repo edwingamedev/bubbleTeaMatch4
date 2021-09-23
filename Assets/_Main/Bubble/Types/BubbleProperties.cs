4f6cb1ecf9a9c6738a7b2754abd3b35b590b1e6a
Edwin Game Dev <edwinjonesholanda@gmail.com>
2021-09-22 21:57:17 -0300

    Created bubble generator + Bubble Shader

---
+++ Assets/_Main/Bubble/Types/BubbleProperties.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Bubble", menuName = "ScriptableObjects/BubbleProperties")]
+public class BubbleProperties : ScriptableObject
+{
+    public Color mainColor;
+    public Color eyeColor;
+}
