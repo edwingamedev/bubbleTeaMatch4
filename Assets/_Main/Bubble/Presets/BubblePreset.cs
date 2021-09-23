e64731d7ec247d9f7f51eb5ed295f521ba45d99c
Edwin Game Dev <edwinjonesholanda@gmail.com>
2021-09-23 18:44:21 -0300

    Refactor for better name convention

--- Assets/_Main/Bubble/Presets/BubblePreset.cs
+++ Assets/_Main/Bubble/Presets/BubblePreset.cs
@@ -2,8 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Bubble", menuName = "ScriptableObjects/BubbleProperties")]
-public class BubbleProperties : ScriptableObject
+[CreateAssetMenu(fileName = "Bubble", menuName = "ScriptableObjects/BubblePreset")]
+public class BubblePreset : ScriptableObject
 {
     public Color mainColor;
     public Color eyeColor;
