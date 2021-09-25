6df10f6a35853592cb63ac0af8db93ede82ab19a
Edwin Game Dev <edwinjonesholanda@gmail.com>
2021-09-25 20:24:20 -0300

    Grid now has bottom reach check + validate when bubble can be assigned + also changed how to assign new bubbles to the grid

--- Assets/_Main/Grid/Grid.cs
+++ Assets/_Main/Grid/Grid.cs
@@ -2,26 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grid
+namespace EdwinGameDev.BubbleTeaMatch4
 {
-    public Bubble[,] cells;
-    public Grid(Vector2Int size)
+    public class Grid
     {
-        cells = new Bubble[size.x, size.y];
-    }
+        public Bubble[,] cells;
+        public Grid(Vector2Int size)
+        {
+            cells = new Bubble[size.x, size.y];
+        }
 
-    public void AssignBubble(int x, int y, Bubble bubble)
-    {
-        cells[x, y] = bubble;
-    }
+        public bool ReachedBottom(Vector2 position)
+        {
+            return position.y <= 0 || cells[Mathf.CeilToInt(position.x), Mathf.CeilToInt(position.y - 1)] != null;
+        }
 
-    public void UnnassignBubble(int x, int y)
-    {
-        var bubble = cells[x, y];
-        cells[x, y] = null;
+        public void AssignBubble(Bubble bubble)
+        {
+            if (bubble.GetPosition().y < cells.GetLength(1))
+                cells[bubble.GetPosition().x, bubble.GetPosition().y] = bubble;
+        }
 
+        public void UnnassignBubble(Vector2Int position)
+        {
+            var bubble = cells[position.x, position.y];
+            cells[position.x, position.y] = null;
 
-        //TODO: Pooling
-        Object.Destroy(bubble);
+            //TODO: Pooling
+            Object.Destroy(bubble);
+        }
     }
 }
