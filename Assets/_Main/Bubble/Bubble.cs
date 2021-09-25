3acaa86b38684cb6700019a926b02351cea55ee0
Edwin Game Dev <edwinjonesholanda@gmail.com>
2021-09-25 20:17:21 -0300

    bubble now has position set and get independent from transform

--- Assets/_Main/Bubble/Bubble.cs
+++ Assets/_Main/Bubble/Bubble.cs
@@ -2,19 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bubble : MonoBehaviour
+namespace EdwinGameDev.BubbleTeaMatch4
 {
-    [SerializeField] private SpriteRenderer spriteRenderer;
-
-    // Start is called before the first frame update
-    void Start()
+    public class Bubble : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private SpriteRenderer spriteRenderer;
+        private Vector2Int position;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public void SetPosition(Vector2Int position)
+        {
+            this.position = position;
+            transform.position = new Vector2(position.x, position.y);
+        }
+
+        public void MoveDirection(Vector2Int direction)
+        {
+            SetPosition(new Vector2Int(Mathf.CeilToInt(transform.position.x) + direction.x, 
+                                        Mathf.CeilToInt(transform.position.y) + direction.y));
+        }
+
+        public Vector2Int GetPosition()
+        {
+            return position;
+        }
     }
 }
