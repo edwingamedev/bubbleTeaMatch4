910ecb03db5f44ec1b0361fa0e136545ccb7a07c
Edwin Game Dev <edwinjonesholanda@gmail.com>
2021-09-25 20:21:41 -0300

    Added next bubble position to game settings

--- Assets/_Main/Settings/GameSettings.cs
+++ Assets/_Main/Settings/GameSettings.cs
@@ -1,20 +1,26 @@
 using System.Collections;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Settings", menuName = "ScriptableObjects/Settings")]
-public class GameSettings : ScriptableObject
+namespace EdwinGameDev.BubbleTeaMatch4
 {
-    [SerializeField] private int cellSize;
-    [SerializeField] private Vector2Int gridSize;
-    [SerializeField] private BubbleSettings bubbleSettings;
-    [SerializeField] private Vector2Int spawnPosition;
-    [SerializeField] private Vector2Int nextBubblePosition;
+    [CreateAssetMenu(fileName = "Settings", menuName = "ScriptableObjects/Settings")]
+    public class GameSettings : ScriptableObject
+    {
+        [SerializeField] private int cellSize;
+        [SerializeField] private Vector2Int gridSize;
+        [SerializeField] private BubbleSettings bubbleSettings;
+        [SerializeField] private Vector2Int spawnPosition;
+        [SerializeField] private Vector2Int nextBubblePosition;
 
-    public int CellSize => cellSize;
-    public Vector2Int GridSize => gridSize;
+        public int CellSize => cellSize;
+        public Vector2Int GridSize => gridSize;
 
-    public BubbleSettings BubbleSettings => bubbleSettings;
+        public BubbleSettings BubbleSettings => bubbleSettings;
 
-    public Vector2Int MainBubbleSpawnPosition => spawnPosition;
-    public Vector2Int SecondaryBubbleSpawnPosition => spawnPosition + Vector2Int.up;
+        public Vector2Int MainBubbleSpawnPosition => spawnPosition;
+        public Vector2Int SubBubbleSpawnPosition => spawnPosition + Vector2Int.up;
+
+        public Vector2Int NextMainBubblePosition => nextBubblePosition;
+        public Vector2Int NextSubBubblePosition => nextBubblePosition + Vector2Int.up;
+    }
 }
