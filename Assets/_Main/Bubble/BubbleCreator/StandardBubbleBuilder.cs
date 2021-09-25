cb75d46e8c22b79ca72498e5d455a33748a174bd
Edwin Game Dev <edwinjonesholanda@gmail.com>
2021-09-25 20:22:50 -0300

    Bubblebuilder now return bubble when generation it

--- Assets/_Main/Bubble/BubbleCreator/StandardBubbleBuilder.cs
+++ Assets/_Main/Bubble/BubbleCreator/StandardBubbleBuilder.cs
@@ -2,28 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StandardBubbleBuilder : IBubbleBuilder
+namespace EdwinGameDev.BubbleTeaMatch4
 {
-    private GameSettings settings;
-    private int bubbleAmount;
-
-    public StandardBubbleBuilder(GameSettings settings)
+    public class StandardBubbleBuilder : IBubbleBuilder
     {
-        this.settings = settings;
-        bubbleAmount = settings.BubbleSettings.BubblePresets.Count;
-    }
+        private GameSettings settings;
+        private int bubbleAmount;
 
+        public StandardBubbleBuilder(GameSettings settings)
+        {
+            this.settings = settings;
+            bubbleAmount = settings.BubbleSettings.BubblePresets.Count;
+        }
 
+        public Bubble Generate(Vector2Int position)
+        {
+            int bubbleIndex = Random.Range(0, bubbleAmount);
 
-    public void Generate(Vector2Int position)
-    {
-        int bubbleIndex = Random.Range(0, bubbleAmount);
+            var go = Object.Instantiate(settings.BubbleSettings.Prefab, (Vector2)position, Quaternion.identity);
+            var spriteRenderer = go.GetComponentInChildren<SpriteRenderer>();
+            spriteRenderer.material = new Material(settings.BubbleSettings.Shader);
+            spriteRenderer.material.SetColor("_MainColor", settings.BubbleSettings.BubblePresets[bubbleIndex].mainColor);
+            spriteRenderer.material.SetColor("_EyeColor", settings.BubbleSettings.BubblePresets[bubbleIndex].eyeColor);
 
-        var go = Object.Instantiate(settings.BubbleSettings.Prefab, (Vector2)position, Quaternion.identity);
-        var spriteRenderer = go.GetComponentInChildren<SpriteRenderer>();
-        spriteRenderer.material = new Material(settings.BubbleSettings.Shader);
-        spriteRenderer.material.SetColor("_MainColor", settings.BubbleSettings.BubblePresets[bubbleIndex].mainColor);
-        spriteRenderer.material.SetColor("_EyeColor", settings.BubbleSettings.BubblePresets[bubbleIndex].eyeColor);
+            var bubble = go.GetComponent<Bubble>();
 
+            return bubble;
+        }
     }
 }
