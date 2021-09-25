fea57cd7e08d8222cc58320e6008b803251f7f28
Edwin Game Dev <edwinjonesholanda@gmail.com>
2021-09-25 20:26:46 -0300

    Created simple game loop that allows player movement + game over check and spawn new bubble sets

--- Assets/_Main/GameManager.cs
+++ Assets/_Main/GameManager.cs
@@ -2,32 +2,165 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+namespace EdwinGameDev.BubbleTeaMatch4
 {
-    [SerializeField] private GameSettings gameSettings;
-    private IGridBuilder gridBuilder;
-    private IBubbleBuilder bubbleBuilder;
-
-    // Start is called before the first frame update
-    void Start()
+    public class GameManager : MonoBehaviour
     {
-        Initialize();
-    }
+        [SerializeField] private GameSettings gameSettings;
+        private IGridBuilder gridBuilder;
+        private IBubbleBuilder bubbleBuilder;
+        private BubbleMovementController bubbleMovementController;
+        private bool isPlaying = false;
 
-    private void Initialize()
-    {
-        bubbleBuilder = new StandardBubbleBuilder(gameSettings);
-        gridBuilder = new StandardGridBuilder(gameSettings);
+        private float dropRate = 1f;
+        private float nextDrop;
 
-        gridBuilder.Build();
+        private Bubble mainBubble;
+        private Bubble subBubble;
 
-        bubbleBuilder.Generate(gameSettings.MainBubbleSpawnPosition);
-        bubbleBuilder.Generate(gameSettings.SecondaryBubbleSpawnPosition);
-    }
+        private Bubble nextMainBubble;
+        private Bubble nextSubBubble;
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Initialize();
+        }
 
+        private void Initialize()
+        {
+            bubbleBuilder = new StandardBubbleBuilder(gameSettings);
+            gridBuilder = new StandardGridBuilder(gameSettings);
+            gridBuilder.Build();
+
+            bubbleMovementController = new BubbleMovementController(gameSettings, gridBuilder.Grid);           
+
+            // Spawn bubble set
+            SpawnNewBubbleSet();
+
+            // Start Game
+            isPlaying = true;
+        }
+
+        private void SpawnNewBubbleSet()
+        {
+            // Spawn new Set
+            if (nextMainBubble == null)
+            {
+                // Spawn main
+                mainBubble = bubbleBuilder.Generate(gameSettings.MainBubbleSpawnPosition);
+                // Spawn sub
+                subBubble = bubbleBuilder.Generate(gameSettings.SubBubbleSpawnPosition);
+            }
+            else
+            {
+                // Spawn main
+                mainBubble = nextMainBubble;
+                // Spawn sub
+                subBubble = nextSubBubble;
+            }
+
+            // Set Initial Position
+            mainBubble.SetPosition(gameSettings.MainBubbleSpawnPosition + Vector2Int.down);
+            subBubble.SetPosition(gameSettings.SubBubbleSpawnPosition + Vector2Int.down);
+
+            bubbleMovementController.SetBubbles(mainBubble, subBubble);
+
+            // Next
+            nextMainBubble = bubbleBuilder.Generate(gameSettings.NextMainBubblePosition);
+            nextSubBubble = bubbleBuilder.Generate(gameSettings.NextSubBubblePosition);
+        }
+
+        private bool BubbleReachedBottom(Bubble bubble)
+        {
+            return gridBuilder.Grid.ReachedBottom(bubble.GetPosition());
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (isPlaying)
+            {
+                BubbleDrop();
+
+                PlayerMovement();
+            }
+        }
+
+        private void BubbleDrop()
+        {
+            if (Time.time > nextDrop)
+            {
+                nextDrop = Time.time + dropRate;
+
+                if (!ReachedBottom())
+                {
+                    bubbleMovementController.MoveDown();
+                }
+            }
+        }
+
+        private void SetGameOver()
+        {
+            isPlaying = false;
+            Debug.Log("GAME OVER");
+        }
+
+        private bool CheckGameOver()
+        {
+            return mainBubble.GetPosition().y >= gameSettings.GridSize.y || subBubble.GetPosition().y >= gameSettings.GridSize.y;
+        }
+
+        private void PlayerMovement()
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                if (!ReachedBottom())
+                {
+                    bubbleMovementController.MoveLeft();
+                }
+            }
+
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                if (!ReachedBottom())
+                {
+                    bubbleMovementController.MoveRight();
+                }
+            }
+
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                if (!ReachedBottom())
+                {
+                    bubbleMovementController.MoveDown();
+                }
+            }
+        }
+
+        private bool ReachedBottom()
+        {
+            if (BubbleReachedBottom(mainBubble) || BubbleReachedBottom(subBubble))
+            {
+                if (CheckGameOver())
+                {
+                    SetGameOver();
+                }
+                else
+                {
+                    // Drop Pieces
+                    gridBuilder.Grid.AssignBubble(mainBubble);
+                    gridBuilder.Grid.AssignBubble(subBubble);
+
+                    SpawnNewBubbleSet();
+                }
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
