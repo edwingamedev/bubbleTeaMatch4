393606b0e52bf4eb2902360881c9a1ae71525d4f
Edwin Game Dev <edwinjonesholanda@gmail.com>
2021-09-27 19:15:05 -0300

    Created simple game state + Created Arrange state

--- Assets/_Main/GameManager.cs
+++ Assets/_Main/GameManager.cs
@@ -10,7 +10,6 @@ namespace EdwinGameDev.BubbleTeaMatch4
         private IGridBuilder gridBuilder;
         private IBubbleBuilder bubbleBuilder;
         private BubbleMovementController bubbleMovementController;
-        private bool isPlaying = false;
 
         private float dropRate = 1f;
         private float nextDrop;
@@ -21,25 +20,59 @@ namespace EdwinGameDev.BubbleTeaMatch4
         private Bubble nextMainBubble;
         private Bubble nextSubBubble;
 
+        private GameState gameState = GameState.Initialize;
+
+        bool isArranging = false;
+
         // Start is called before the first frame update
         void Start()
         {
             Initialize();
         }
 
+        // Update is called once per frame
+        void Update()
+        {
+            switch (gameState)
+            {
+                case GameState.Initialize:
+                    Initialize();
+                    break;
+                case GameState.Creating:
+                    SpawnNewBubbleSet();
+                    break;
+                case GameState.Playing:
+                    BubbleDrop();
+                    PlayerMovement();
+                    break;
+                case GameState.Arrange:
+                    if (!isArranging)
+                        StartCoroutine(ArrangeBubbles());
+                    break;
+                case GameState.Linking:
+                    gameState = GameState.Creating;
+                    break;
+                case GameState.Combo:
+                    break;
+                case GameState.Pause:
+                    break;
+                default:
+                    break;
+            }
+        }
+
         private void Initialize()
         {
+            gameState = GameState.Initialize;
+
             bubbleBuilder = new StandardBubbleBuilder(gameSettings);
             gridBuilder = new StandardGridBuilder(gameSettings);
             gridBuilder.Build();
 
-            bubbleMovementController = new BubbleMovementController(gameSettings, gridBuilder.Grid);           
-
-            // Spawn bubble set
-            SpawnNewBubbleSet();
+            bubbleMovementController = new BubbleMovementController(gameSettings, gridBuilder.Grid);
 
             // Start Game
-            isPlaying = true;
+            gameState = GameState.Creating;
         }
 
         private void SpawnNewBubbleSet()
@@ -48,9 +81,9 @@ namespace EdwinGameDev.BubbleTeaMatch4
             if (nextMainBubble == null)
             {
                 // Spawn main
-                mainBubble = bubbleBuilder.Generate(gameSettings.MainBubbleSpawnPosition);
+                mainBubble = bubbleBuilder.Generate(gameSettings.MainBubbleSpawnPosition, true);
                 // Spawn sub
-                subBubble = bubbleBuilder.Generate(gameSettings.SubBubbleSpawnPosition);
+                subBubble = bubbleBuilder.Generate(gameSettings.SubBubbleSpawnPosition, false);
             }
             else
             {
@@ -67,24 +100,17 @@ namespace EdwinGameDev.BubbleTeaMatch4
             bubbleMovementController.SetBubbles(mainBubble, subBubble);
 
             // Next
-            nextMainBubble = bubbleBuilder.Generate(gameSettings.NextMainBubblePosition);
-            nextSubBubble = bubbleBuilder.Generate(gameSettings.NextSubBubblePosition);
+            nextMainBubble = bubbleBuilder.Generate(gameSettings.NextMainBubblePosition, true);
+            nextSubBubble = bubbleBuilder.Generate(gameSettings.NextSubBubblePosition, false);
+
+
+            // Start Game
+            gameState = GameState.Playing;
         }
 
         private bool BubbleReachedBottom(Bubble bubble)
         {
             return gridBuilder.Grid.ReachedBottom(bubble.GetPosition());
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-            if (isPlaying)
-            {
-                BubbleDrop();
-
-                PlayerMovement();
-            }
         }
 
         private void BubbleDrop()
@@ -102,7 +128,7 @@ namespace EdwinGameDev.BubbleTeaMatch4
 
         private void SetGameOver()
         {
-            isPlaying = false;
+            gameState = GameState.Pause;
             Debug.Log("GAME OVER");
         }
 
@@ -113,29 +139,63 @@ namespace EdwinGameDev.BubbleTeaMatch4
 
         private void PlayerMovement()
         {
-            if (Input.GetKey(KeyCode.LeftArrow))
+            if (!ReachedBottom())
             {
-                if (!ReachedBottom())
+                if (Input.GetKeyDown(KeyCode.LeftArrow))
                 {
                     bubbleMovementController.MoveLeft();
                 }
-            }
 
-            if (Input.GetKey(KeyCode.RightArrow))
-            {
-                if (!ReachedBottom())
+                if (Input.GetKeyDown(KeyCode.RightArrow))
                 {
                     bubbleMovementController.MoveRight();
                 }
-            }
 
-            if (Input.GetKey(KeyCode.DownArrow))
-            {
-                if (!ReachedBottom())
+                if (Input.GetKeyDown(KeyCode.DownArrow))
                 {
                     bubbleMovementController.MoveDown();
                 }
+
+                if (Input.GetKeyDown(KeyCode.Z))
+                {
+                    bubbleMovementController.TurnClockwise();
+                }
+
+                if (Input.GetKeyDown(KeyCode.X))
+                {
+                    bubbleMovementController.TurnCounterClockwise();
+                }
             }
+        }
+
+        private IEnumerator ArrangeBubbles()
+        {
+            isArranging = true;
+
+            for (int y = 1; y < gameSettings.GridSize.y; y++)
+            {
+                for (int x = 0; x < gameSettings.GridSize.x; x++)
+                {
+                    if (gridBuilder.Grid.cells[x, y] != null)
+                    {
+                        if (gridBuilder.Grid.cells[x, y - 1] == null)
+                        {
+                            Bubble bubble = gridBuilder.Grid.cells[x, y];
+                            bubble.MoveDirection(Vector2Int.down);
+                            gridBuilder.Grid.cells[x, y - 1] = gridBuilder.Grid.cells[x, y];
+                            gridBuilder.Grid.cells[x, y] = null;
+                            y = 1;
+                            x = -1;
+                        }
+                    }
+                }
+
+                yield return new WaitForEndOfFrame();
+            }
+
+            isArranging = false;
+
+            gameState = GameState.Linking;
         }
 
         private bool ReachedBottom()
@@ -152,7 +212,7 @@ namespace EdwinGameDev.BubbleTeaMatch4
                     gridBuilder.Grid.AssignBubble(mainBubble);
                     gridBuilder.Grid.AssignBubble(subBubble);
 
-                    SpawnNewBubbleSet();
+                    gameState = GameState.Arrange;
                 }
 
                 return true;
---
+++ Assets/_Main/GameState.cs
@@ -0,0 +1,13 @@
+﻿namespace EdwinGameDev.BubbleTeaMatch4
+{
+    public enum GameState
+    {
+        Initialize,
+        Creating,
+        Playing,
+        Arrange,
+        Linking,
+        Combo,
+        Pause
+    }
+}
