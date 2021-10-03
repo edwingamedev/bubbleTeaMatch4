0271fefa1ccee85c1324e8c0b73d8e6177a79d8b
Edwin Game Dev <edwinjonesholanda@gmail.com>
2021-10-02 22:48:25 -0300

    Created PlayingState

---
+++ Assets/_Main/StateMachine/PlayingState.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace EdwinGameDev.BubbleTeaMatch4
+{
+    public class PlayingState : IState
+    {
+        private GameBoard gameVariables;
+
+        private float dropRate = 1f;
+        private float nextDrop;
+
+        public PlayingState(GameBoard gameVariables)
+        {
+            this.gameVariables = gameVariables;
+        }
+
+        public void OnEnter() { }
+
+        public void OnExit() { }
+
+        public void Tick()
+        {
+            BubbleDrop();
+            CheckInputs();
+        }
+
+        private void CheckInputs()
+        {
+            if (gameVariables.gridBehaviour.ValidateBubbleMovement(gameVariables.bubbleSpawner.CurrentSet))
+            {
+                gameVariables.inputController.CheckInputs();
+            }
+        }
+
+        private void BubbleDrop()
+        {
+            if (Time.time > nextDrop)
+            {
+                nextDrop = Time.time + dropRate;
+
+                // Validate Bubble Drop
+                if (gameVariables.gridBehaviour.ValidateBubbleMovement(gameVariables.bubbleSpawner.CurrentSet))
+                {
+                    // Move down
+                    gameVariables.inputController.ValidateAndMoveDown();
+                }
+            }
+        }
+    }
+}
