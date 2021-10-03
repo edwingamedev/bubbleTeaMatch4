5a020f9baac26541c2d6868314ce88fe5ce5aea4
Edwin Game Dev <edwinjonesholanda@gmail.com>
2021-10-02 22:47:45 -0300

    Created StartState

---
+++ Assets/_Main/StateMachine/StartState.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace EdwinGameDev.BubbleTeaMatch4
+{
+    public class StartState : IState
+    {
+        public void OnEnter()
+        {
+            Debug.Log("START GAME");
+        }
+
+        public void OnExit()
+        {
+
+        }
+
+        public void Tick()
+        {
+
+        }
+    }
+}
