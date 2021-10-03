dde149ad7cbbba37bf3de7dcfcfe35ba8d136648
Edwin Game Dev <edwinjonesholanda@gmail.com>
2021-10-02 22:47:56 -0300

    Created GameOverState

---
+++ Assets/_Main/StateMachine/GameOverState.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace EdwinGameDev.BubbleTeaMatch4
+{
+    public class GameOverState : IState
+    {
+        public void OnEnter()
+        {
+            Debug.Log("GAME OVER");
+        }
+
+        public void OnExit() { }
+
+        public void Tick() { }
+    }
+}
