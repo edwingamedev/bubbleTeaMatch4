915208e994395266afb94b5394a616fc24ebc619
Edwin Game Dev <edwinjonesholanda@gmail.com>
2021-10-02 22:49:12 -0300

    Created SpawnState

---
+++ Assets/_Main/StateMachine/SpawnState.cs
@@ -0,0 +1,28 @@
+﻿namespace EdwinGameDev.BubbleTeaMatch4
+{
+    public class SpawnState : IState
+    {
+        private GameBoard gameVariables;
+
+        public SpawnState(GameBoard gameVariables)
+        {
+            this.gameVariables = gameVariables;
+        }
+
+        public void OnEnter()
+        {
+            //Spawn new bubble set
+            gameVariables.bubbleSpawner.SpawnNewBubbleSet();
+
+            // Update controlled bubbles
+            gameVariables.inputController.SetBubbles(gameVariables.bubbleSpawner.CurrentSet);
+
+            UnityEngine.Debug.Log("Spawned");
+
+        }
+
+        public void OnExit() { }
+
+        public void Tick() { }
+    }
+}
