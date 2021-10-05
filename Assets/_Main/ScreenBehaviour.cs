f2cb7411bc8e6addaf026cc78d058a343f14f01f
Edwin Game Dev <edwinjonesholanda@gmail.com>
2021-10-05 01:12:37 -0300

    Created screen manager + screen behaviour

---
+++ Assets/_Main/ScreenBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace EdwinGameDev.BubbleTeaMatch4
+{
+    [System.Serializable]
+    public abstract class ScreenBehaviour : MonoBehaviour
+    {
+        public abstract void OnActivate();
+
+        public abstract void OnDeactivate();
+    }
+}
---
+++ Assets/_Main/ScreenManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace EdwinGameDev.BubbleTeaMatch4
+{
+    public static class ScreenManager
+    {
+        private static List<ScreenBehaviour> screenBehaviours = new List<ScreenBehaviour>();
+        public static ScreenBehaviour currentScreen;
+
+        public static void AssignScreen(ScreenBehaviour screenBehaviour)
+        {
+            if (!screenBehaviours.Contains(screenBehaviour))
+            {
+                screenBehaviours.Add(screenBehaviour);
+                screenBehaviour.OnDeactivate();
+            }                
+        }
+
+        public static void UnassignScreen(ScreenBehaviour screenBehaviour)
+        {
+            if (screenBehaviours.Contains(screenBehaviour))
+                screenBehaviours.Remove(screenBehaviour);
+        }
+
+        public static void LoadScreen(Type screenType)
+        {
+            var screen = screenBehaviours.Find((screen) => screen.GetType() == screenType);
+
+            if (screen)
+            {
+                // Disable previous screen
+                currentScreen?.OnDeactivate();
+
+                // Enable new screen
+                screen.OnActivate();
+                currentScreen = screen;
+            }
+        }
+    }
+}
