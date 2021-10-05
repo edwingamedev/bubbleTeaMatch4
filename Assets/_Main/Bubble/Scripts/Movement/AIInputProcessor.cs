23b233f4d6b40dc90e4150d0be9daa6805035c84
Edwin Game Dev <edwinjonesholanda@gmail.com>
2021-10-05 17:48:58 -0300

    Created simple random AI

---
+++ Assets/_Main/Bubble/Scripts/Movement/AIInputProcessor.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Threading.Tasks;
+
+namespace EdwinGameDev.BubbleTeaMatch4
+{
+    public class AIInputProcessor : IInputProcessor
+    {
+        private bool moveDown;
+        private bool moveLeft;
+        private bool moveRight;
+        private bool turnCounterClockwise;
+        private bool turnClockwise;
+
+        private int delayBetweenDecisions = 500;
+
+        public AIInputProcessor()
+        {
+            RandomDecision();
+        }
+
+        private async void RandomDecision()
+        {
+            await Task.Delay(delayBetweenDecisions);
+
+            int random = Random.Range(0, 5);
+
+            switch (random)
+            {
+                case 0:
+                    moveDown = true;
+                    break;
+                case 1:
+                    moveLeft = true;
+                    break;
+                case 2:
+                    moveRight = true;
+                    break;
+                case 3:
+                    turnClockwise = true;
+                    break;
+                case 4:
+                    turnCounterClockwise = true;
+                    break;
+                default:
+                    break;
+            }            
+
+            RandomDecision();
+        }
+
+        public bool Down()
+        {
+            if (moveDown)
+            {
+                moveDown = false;
+                return true;
+            }                
+
+            return false;
+        }
+
+        public bool Left()
+        {
+            if (moveLeft)
+            {
+                moveLeft = false;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool Right()
+        {
+            if (moveRight)
+            {
+                moveRight = false;
+                return true;
+            }
+                
+            return false;
+        }
+
+        public bool TurnClockwise()
+        {
+            if (turnClockwise)
+            {
+                turnClockwise = false;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool TurnCounterClockwise()
+        {
+            if (turnCounterClockwise)                
+            {
+                turnCounterClockwise = false;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
