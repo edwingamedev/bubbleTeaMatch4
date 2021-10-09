7dcca62cc16ac636f79112b2ca0286afffa37df4
Edwin Game Dev <edwinjonesholanda@gmail.com>
2021-10-09 01:47:38 -0300

    Create pool provider

---
+++ Assets/_Main/PoolProvider.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace EdwinGameDev.BubbleTeaMatch4
+{
+    public class PoolProvider
+    {
+        public Pooling CreateBubblePool(IPool pool, string name, int amount)
+        {
+            var go = new GameObject();
+            go.name = name;
+            var p = new Pooling(go.transform);
+            p.CreatePool(pool, amount);
+
+            return p;
+        }
+    }
+}
