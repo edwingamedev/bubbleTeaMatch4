7acd326594203798c5d854ff270cc8b83a3122dc
Edwin Game Dev <edwinjonesholanda@gmail.com>
2021-10-11 00:52:53 -0300

    Refactor keyboard input processor

--- Assets/_Main/Bubble/Scripts/Movement/KeyboardInputProcessor.cs
+++ Assets/_Main/Bubble/Scripts/Movement/KeyboardInputProcessor.cs
@@ -1,31 +1,81 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace EdwinGameDev.BubbleTeaMatch4
 {
-
     public class KeyboardInputProcessor : IInputProcessor
     {
-        public bool Down()
+        public Action OnTurnClockwise { get; set; }
+        public Action OnTurnCounterClockwise { get; set; }
+        public Action<Vector2Int> OnMove { get; set; }
+
+
+        private float inputDelay = .1f;
+        private float nextInput;
+        private Vector2Int moveVector;
+
+        public void CheckInputs()
+        {
+            // Movement
+            if (Time.time > nextInput)
+            {
+                nextInput = Time.time + inputDelay;
+
+                if (Left())
+                    moveVector += Vector2Int.left;
+
+                if (Right())
+                    moveVector += Vector2Int.right;
+
+                if (Down())
+                    moveVector += Vector2Int.down;
+
+                // Move
+                if (moveVector != Vector2Int.zero)
+                {
+                    // Move vertical
+                    OnMove(new Vector2Int(0, moveVector.y));
+
+                    // Move horizontal
+                    OnMove(new Vector2Int(moveVector.x, 0));
+                }
+
+                // Reset movement
+                moveVector = Vector2Int.zero;
+            }
+
+            // Turn validation
+            if (TurnClockwise())
+            {
+                OnTurnClockwise();
+            }
+            else if (TurnCounterClockwise())
+            {
+                OnTurnCounterClockwise();
+            }
+        }
+
+        private bool Down()
         {
             return Input.GetButton("Vertical") && Input.GetAxis("Vertical") < 0;
         }
 
-        public bool Left()
+        private bool Left()
         {
             return Input.GetButton("Horizontal") && Input.GetAxis("Horizontal") < 0;
         }
 
-        public bool Right()
+        private bool Right()
         {
             return Input.GetButton("Horizontal") && Input.GetAxis("Horizontal") > 0;
         }
 
-        public bool TurnClockwise()
+        private bool TurnClockwise()
         {
             return Input.GetKeyDown(KeyCode.C);
         }
 
-        public bool TurnCounterClockwise()
+        private bool TurnCounterClockwise()
         {
             return Input.GetKeyDown(KeyCode.X);
         }
