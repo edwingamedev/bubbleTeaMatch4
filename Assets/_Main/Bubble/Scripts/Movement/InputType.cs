d735da3808da232b4b128f4d182f44509f28109d
Edwin Game Dev <edwinjonesholanda@gmail.com>
2021-10-11 00:52:04 -0300

    Created touch input processor

---
+++ Assets/_Main/Bubble/Scripts/Movement/InputType.cs
@@ -0,0 +1,13 @@
+﻿namespace EdwinGameDev.BubbleTeaMatch4
+{
+    public enum InputType
+    {
+        NoInput,
+        Release,
+        Click,
+        Up,
+        Down,
+        Left,
+        Right
+    }
+}
---
+++ Assets/_Main/Bubble/Scripts/Movement/TouchInputProcessor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+namespace EdwinGameDev.BubbleTeaMatch4
+{
+    public class TouchInputProcessor : IInputProcessor
+    {
+        private TouchInputProvider touchInputProcessor;
+        public Action OnTurnClockwise { get; set; }
+        public Action OnTurnCounterClockwise { get; set; }
+        public Action<Vector2Int> OnMove { get; set; }
+
+        public TouchInputProcessor()
+        {
+            touchInputProcessor = new TouchInputProvider();
+        }
+
+        public void CheckInputs()
+        {
+            var touch = touchInputProcessor.DetectTouch();
+
+            switch (touch)
+            {
+                case InputType.Down:
+                    Debug.Log("swipe " + touch);
+                    OnMove(Vector2Int.down);
+                    break;
+                case InputType.Left:
+                    Debug.Log("swipe " + touch);
+                    OnMove(Vector2Int.left);
+                    break;
+                case InputType.Right:
+                    Debug.Log("swipe " + touch);
+                    OnMove(Vector2Int.right);
+                    break;
+                case InputType.Release:
+                    Debug.Log("click " + touch);
+
+                    if (Input.GetTouch(0).position.x < Screen.width)
+                        OnTurnCounterClockwise();
+                    else
+                        OnTurnClockwise();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
