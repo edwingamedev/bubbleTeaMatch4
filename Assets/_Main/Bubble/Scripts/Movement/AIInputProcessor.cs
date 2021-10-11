f57f766eafbdc1bd2f5f9e17da962c0d03297133
Edwin Game Dev <edwinjonesholanda@gmail.com>
2021-10-11 00:52:58 -0300

    Refactor AI input processor

--- Assets/_Main/Bubble/Scripts/Movement/AIInputProcessor.cs
+++ Assets/_Main/Bubble/Scripts/Movement/AIInputProcessor.cs
@@ -1,106 +1,53 @@
 ﻿using UnityEngine;
 using System.Threading.Tasks;
+using System;
 
 namespace EdwinGameDev.BubbleTeaMatch4
 {
     public class AIInputProcessor : IInputProcessor
     {
-        private bool moveDown;
-        private bool moveLeft;
-        private bool moveRight;
-        private bool turnCounterClockwise;
-        private bool turnClockwise;
+        public Action OnTurnClockwise { get; set; }
+        public Action OnTurnCounterClockwise { get; set; }
+        public Action<Vector2Int> OnMove { get; set; }
 
-        private int delayBetweenDecisions = 500;
+        private float inputDelay = .5f;
+        private float nextInput;
+        
 
-        public AIInputProcessor()
+        public void CheckInputs()
         {
-            RandomDecision();
+            if (Time.time > nextInput)
+            {
+                nextInput = Time.time + inputDelay;
+
+                RandomDecision();
+            }
         }
 
-        private async void RandomDecision()
-        {
-            await Task.Delay(delayBetweenDecisions);
-
-            int random = Random.Range(0, 5);
+        private void RandomDecision()
+        {            
+            int random = UnityEngine.Random.Range(0, 5);
 
             switch (random)
             {
                 case 0:
-                    moveDown = true;
+                    OnMove(Vector2Int.down);
                     break;
                 case 1:
-                    moveLeft = true;
+                    OnMove(Vector2Int.left);
                     break;
                 case 2:
-                    moveRight = true;
+                    OnMove(Vector2Int.right);
                     break;
                 case 3:
-                    turnClockwise = true;
+                    OnTurnClockwise();
                     break;
                 case 4:
-                    turnCounterClockwise = true;
+                    OnTurnCounterClockwise();
                     break;
                 default:
                     break;
-            }            
-
-            RandomDecision();
-        }
-
-        public bool Down()
-        {
-            if (moveDown)
-            {
-                moveDown = false;
-                return true;
-            }                
-
-            return false;
-        }
-
-        public bool Left()
-        {
-            if (moveLeft)
-            {
-                moveLeft = false;
-                return true;
             }
-
-            return false;
-        }
-
-        public bool Right()
-        {
-            if (moveRight)
-            {
-                moveRight = false;
-                return true;
-            }
-                
-            return false;
-        }
-
-        public bool TurnClockwise()
-        {
-            if (turnClockwise)
-            {
-                turnClockwise = false;
-                return true;
-            }
-
-            return false;
-        }
-
-        public bool TurnCounterClockwise()
-        {
-            if (turnCounterClockwise)                
-            {
-                turnCounterClockwise = false;
-                return true;
-            }
-
-            return false;
         }
     }
 }
